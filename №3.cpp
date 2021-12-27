62aa8947c2d92d95709b80f0604b5f7f70fcf369
MarinaToptygina <91845711+MarinaToptygina@users.noreply.github.com>
2021-12-27 22:08:08 +0300

    Update №3.cpp

--- №3.cpp
+++ №3.cpp
@@ -21,6 +21,16 @@ void random_array(int** array, int n, int m) {
 	}
 }
 
+void swap_relative_to_side_diagonal(int** mas, int n, int m){
+	int c = 0;
+	for (int i = 0; i < n; i++)
+		for(int j = 0; j <  n - i - 1; j++){
+			c = mas[i][j];
+			mas[i][j]=mas[n - j - 1][n - i - 1];
+			mas[n - j - 1][n - i - 1] = c;
+		}
+}
+
 int main() {
 	int n, m;                              		
 	cin >> n >> m;
@@ -29,23 +39,14 @@ int main() {
 		mas[i] = new int[m];               
 	}
 	random_array(mas, n, m);
-	cout << "Matrix A: " << endl;
-
-	print_array(mas, n, m);
-
-	int c = 0;
-	for (int i = 0; i < n; i++)
-		for (int j = 0; j < n - i - 1; j++) {
-			c = mas[i][j];
-			mas[i][j] = mas[n - j - 1][n - i - 1];
-			mas[n - j - 1][n - i - 1] = c;
-		}
+	cout << "Matrix A: " << endl; 
+	
+	print_array(mas, n, m);	
+	
+	swap_relative_to_side_diagonal(mas, n, m)
+		
 	cout << "Matrix B: " << endl;
-
 	print_array(mas, n, m);
-
-
-
 	for (int i = 0; i < n; i++)
 		delete[] mas[i];         
 	delete[] mas;    
